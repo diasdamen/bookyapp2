09453ddea43c0892f23ae7a986ffb4be0a5fcacd
Dias Damen <diasdamen@gmail.com>
2024-07-25 13:18:23 +0500

    database upd3

--- config/database.js
+++ config/database.js
@@ -51,7 +51,7 @@ module.exports = ({ env }) => {
       connection: {
         connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT', 5432),
+        port: env.int('DATABASE_PORT'),
         database: env('DATABASE_NAME'),
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
